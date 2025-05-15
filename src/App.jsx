f7c0838580bc4b447f2ac4dc03e1656dec0a0c4a
harsh8439 <harshsharma@pearlorganisation.com>
2025-05-15 16:27:43 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AdminPanel from "./Pages/AdminPanel/AdminPanel";
 import Disclaimer from "./Pages/Disclaimer/Disclaimer";
 import Privacypolicy from "./Pages/Privacypolicy/Privacy";
 import Contact from "./Pages/Contact.us/Contact";
+import PrivateRoute from "./components/PrivateRoute"; // import your private route
 
 const App = () => {
   return (
@@ -14,7 +15,14 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<Signup />} />
-        <Route path="/" element={<AdminPanel />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <AdminPanel />
+            </PrivateRoute>
+          }
+        />
         <Route path="/faq" element={<Faq />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Disclaimer" element={<Disclaimer />} />
